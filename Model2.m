70d1a919233109b73eabff7b59726ed11f7fb7cd
StormMata <storm.mata097@gmail.com>
2022-06-23 23:44:59 -0400

    23-Jun-22; 23:45

--- Model2.m
+++ Model2.m
@@ -1,5 +1,5 @@
-clearvars -except I AeroDyn A Output Hannah HannahSS SSs SSsn speed uRel vRel Hphi aoa Hchord Htwist Hpower HW sweep
-clc
+clearvars -except reserve I AeroDyn A Output Hannah HannahSS SSs SSsn speed uRel vRel Hphi aoa Hchord Htwist Hpower HW sweep
+% clc
 
 addpath(['/Users/stormmata/Library/Mobile Documents/com~apple~CloudDocs/' ...
          'Courses/Research/WindPowerModel/5 MW Turbine']);
@@ -8,23 +8,25 @@ addpath(['/Users/stormmata/Library/Mobile Documents/com~apple~CloudDocs/' ...
 
 load('Turbine.mat')                                                         % Load turbine parameters
 
-load('Airfoils.mat')                                                        % Load airfoil parameters
-
-% load('WindProfiles.mat')                                                    % Load atmospheric conditions
+% load('Airfoils.mat')                                                        % Load airfoil parameters
+A=reserve;
+load('WindProfiles.mat')                                                    % Load atmospheric conditions
 
 %% Interpolate Blade Parameters
 
-R         = 1.5:1:T.R;                                                      % Evenly spaced radial positions    [m]
-
-Chord     = interp1(AeroDyn.r,AeroDyn.Chord,R,'linear','extrap');           % Interpolated chord length         [m]
-
-Twist     = interp1(AeroDyn.r,AeroDyn.Twist,R,'linear','extrap');           % Interpolated twist angle          [deg]
-
-AeroIndex = interp1(AeroDyn.r,AeroDyn.AeroIndex,R,'nearest','extrap');      % Interpolated airfoil index        [-]
+% R         = 1.5:1:T.R;                                                      % Evenly spaced radial positions    [m]
+% 
+% Chord     = interp1(AeroDyn.r,AeroDyn.Chord,R,'linear','extrap');           % Interpolated chord length         [m]
+% 
+% Twist     = interp1(AeroDyn.r,AeroDyn.Twist,R,'linear','extrap');           % Interpolated twist angle          [deg]
+% 
+% AeroIndex = interp1(AeroDyn.r,AeroDyn.AeroIndex,R,'nearest','extrap');      % Interpolated airfoil index        [-]
 
 %% Mesh Rotor Area
 
-[ycoords,zcoords] = meshgrid(-T.R:T.R,(T.Hub+T.R):-1:(T.Hub-T.R));          % Generate grid of points in cartesian coordinates
+spacing = 1;
+
+[ycoords,zcoords] = meshgrid(-T.R:spacing:T.R,(T.Hub+T.R):-spacing:(T.Hub-T.R));          % Generate grid of points in cartesian coordinates
 
 [azimuth,R]       = cart2pol(ycoords,zcoords-T.Hub);                        % Convert to polar coordinates
 
@@ -32,29 +34,76 @@ azimuth           = fliplr(mod(azimuth - pi/2,2*pi));                       % Fi
 
 %% Generate Freestream Velocity Field
 
-a = 1/3;
+a         = 1/3;
+rho       = 1.225;
 
-U      = interp1(I.Heights,I.SpeedShear,((T.Hub+T.R):-1:(T.Hub-T.R))','linear') .* ones(size(azimuth));
+for i = 1:size(I.SpeedShear,2)
+    i
 
-LocYaw = deg2rad(interp1(I.Heights,I.DirShear,((T.Hub+T.R):-1:(T.Hub-T.R))','linear') .* ones(size(azimuth)));
+U         = interp1(I.Heights,I.SpeedShear(:,i),((T.Hub+T.R):-spacing:(T.Hub-T.R))','linear') .* ones(size(azimuth));
 
-U_axi  = (U*(1-a) .* cos(LocYaw .* sin(azimuth)) .* cos(LocYaw .* cos(azimuth)));
+LocYaw    = deg2rad(interp1(I.Heights,I.DirShear(:,i),((T.Hub+T.R):-spacing:(T.Hub-T.R))','linear') .* ones(size(azimuth)));
 
-U_tan  = (T.Omega .* R - U*(1-a) .* cos(LocYaw * sin(azimuth)) .* sin(LocYaw .* cos(azimuth)));
+U_axi     = (U*(1-a) .* cos(LocYaw .* sin(azimuth)) .* cos(LocYaw .* cos(azimuth)));
 
-W      = U_axi.^2 + U_tan.^2;
+U_tan     = (T.Omega .* R - U*(1-a) .* cos(LocYaw * sin(azimuth)) .* sin(LocYaw .* cos(azimuth)));
 
-Phi    = atan2(U_axi, U_tan);
+U_rel     = U_axi.^2 + U_tan.^2;
 
-Chord  = interp1(AeroDyn.r,AeroDyn.Chord,R,'linear','extrap');
+Phi       = atan2(U_axi, U_tan);
+
+Chord     = interp1(AeroDyn.r,AeroDyn.Chord,R,'linear','extrap');
 
 Twist     = interp1(AeroDyn.r,AeroDyn.Twist,R,'linear','extrap');
 
 AeroIndex = interp1(AeroDyn.r,AeroDyn.AeroIndex,R,'nearest','extrap');      % Interpolated airfoil index        [-]
 
-alpha  = rad2deg(Phi) - Twist;
+alpha     = rad2deg(Phi) - Twist;
 
+for m = 1:size(AeroIndex,1)
+    for n = 1:size(AeroIndex,2)
+        CL(m,n) = interp1(A.(sprintf('A%i',AeroIndex(m,n))).Alpha,A.(sprintf('A%i',AeroIndex(m,n))).CL,alpha(m,n),'linear');
+        CD(m,n) = interp1(A.(sprintf('A%i',AeroIndex(m,n))).Alpha,A.(sprintf('A%i',AeroIndex(m,n))).CD,alpha(m,n),'linear');
+    end
+end
 
+Q = T.B .* 0.5 .* rho .* Chord .* U_rel .* R .* (CL .* sin(Phi) - CD .* cos(Phi));
+
+P = T.Omega .* Q;
+
+OutterFilter = R >= T.R_hub;   % Exclude hub area
+
+InnerFilter  = R <= T.R;   % Only capture values within the outer rotor radius
+
+P = P .* OutterFilter .* InnerFilter;
+
+ri     = linspace(1.5,T.R,length(R));
+thetai = linspace(0,2*pi,length(R));
+
+Power(i) = trapz(thetai,trapz(ri,P,2))/(2*pi);
+
+end
+
+% P(P==0) = NaN;
+% 
+% % trapz(linspace(-T.R,T.R,length(R)),trapz(linspace(-T.R,T.R,length(R)),P))/(pi*T.R^2-pi*T.R_hub^2)
+% 
+% trapz(linspace(1,T.R,length(R)),trapz(linspace(T.Hub-T.R,T.Hub+T.R,length(R)),P,1),2)/(pi*T.R^2-pi*T.R_hub^2)
+% 
+% trapz(linspace(-T.R,T.R,length(R)),trapz(linspace(-T.R,T.R,length(R)),P,1),2)/(pi*T.R^2-pi*T.R_hub^2)
+% 
+% trapz(linspace(T.Hub+T.R,T.Hub-T.R,length(R))',P,1)/(pi*T.R^2-pi*T.R_hub^2)
+% 
+% trapz(trapz(P.*azimuth,2).*R,1)/(pi*T.R^2-pi*T.R_hub^2)
+
+% ri     = linspace(1.5,T.R,length(R));
+% thetai = linspace(0,2*pi,length(R));
+% 
+% trapz(thetai,trapz(ri,P,2))/(2*pi)
+
+% 2*pi*trapz(ri,ri.*trapz(thetai,P,1))/(pi*T.R^2-pi*T.R_hub^2)
+
+% P = sum(P,'all')*2*pi/(pi*T.R^2-pi*T.R_hub^2)
 
 % plot(AeroDyn.r,AeroDyn.Chord,'b')
 % hold on
